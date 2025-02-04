850d3403accdd6f70476085c8639cc0eed87677c
milanuhrin <milan.uhrin19@gmail.com>
2025-02-04 13:43:41 +0100

    login update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  AnimOnScroll,
-  Footer,
-  Landing,
-} from 'Components/export';
-import SEO from 'Components/seo.js';
-import '../../global.css';
-import About from 'Components/About';
-import Availability from 'Components/Availability';
-import Gallery from 'Components/Gallery';
-import Pricelist from 'Components/Pricelist';
-import Reservation from 'Components/Reservation';
-import Contact from 'Components/Contact';
-
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AnimOnScroll, Footer, Landing } from "Components/export";
+import SEO from "Components/seo.js";
+import "../../global.css";
+import About from "Components/About";
+import Availability from "Components/Availability";
+import Gallery from "Components/Gallery";
+import Pricelist from "Components/Pricelist";
+import Reservation from "Components/Reservation";
+import Contact from "Components/Contact";
+import Login from "Components/Login"; // New Login Page
+import Admin from "Components/Admin"; // New Admin Page
 
 const IndexPage = () => {
-  let width = 0;
-  if (typeof window !== 'undefined') {
-    const { innerWidth } = window;
-    width = innerWidth;
-  }
-
   return (
-    <>
+    <Router>
       <SEO />
-      <Landing />
-      <About />
-      <Gallery />
-      <Pricelist />
-      <Availability />
-      <Reservation />
-      <Contact />
-      <Footer />
-    </>
+      <Routes>
+        {/* Main Website */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Landing />
+              <About />
+              <Gallery />
+              <Pricelist />
+              <Availability />
+              <Reservation />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+        {/* Admin Page */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 };
 
