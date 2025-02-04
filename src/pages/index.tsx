8565db336701e731b586c696db85b6e6e1d7a908
milanuhrin <milan.uhrin19@gmail.com>
2025-02-04 15:16:01 +0100

    index-login update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
 import { AnimOnScroll, Footer, Landing } from "Components/export";
 import "../../global.css";
 import About from "Components/About";
@@ -8,42 +7,19 @@ import Gallery from "Components/Gallery";
 import Pricelist from "Components/Pricelist";
 import Reservation from "Components/Reservation";
 import Contact from "Components/Contact";
-import Login from "Components/Login";
-import Admin from "Components/Admin";
 import SEO from "Components/SEO";
 
 const IndexPage = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true); // Runs only on the client
-  }, []);
-
-  if (!isClient) {
-    return null; // Prevents SSR from rendering the page
-  }
-
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Landing />
-              <About />
-              <Gallery />
-              <Pricelist />
-              <Availability />
-              <Reservation />
-              <Contact />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <Landing />
+      <About />
+      <Gallery />
+      <Pricelist />
+      <Availability />
+      <Reservation />
+      <Contact />
+      <Footer />
     </>
   );
 };
---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Enter your credentials to access the admin panel.</p>
+    </div>
+  );
+};
+
+export default Login;
