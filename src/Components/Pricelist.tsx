768fbcf9ce761e0afa260105e11bafeaab1396f1
milanuhrin <milan.uhrin19@gmail.com>
2025-02-07 12:01:05 +0100

    pricelist update

--- src/Components/Pricelist.tsx
+++ src/Components/Pricelist.tsx
@@ -7,7 +7,7 @@ const Pricelist = () => {
   return (
     <motion.section
       id="pricelist"
-      className="text-center py-8 max-w-screen-lg mx-auto"
+      className="text-center py-8"
       initial="offscreen"
       whileInView="onscreen"
       viewport={{ once: true, amount: 0.01 }}
@@ -20,22 +20,18 @@ const Pricelist = () => {
 
       {/* Table */}
       <motion.div
-        className="overflow-x-auto sm:overflow-x-hidden px-4 sm:px-6 lg:px-8 mt-8"
+        className="overflow-x-auto sm:overflow-x-hidden px-4 sm:px-6 lg:px-8 mt-8 "
         variants={sectionVariants}
       >
         <div className="max-w-lg mx-auto lg:max-w-[700px] border border-gray-300 rounded-lg shadow-lg overflow-hidden">
           <table className="w-full border-collapse text-gray-500">
             <thead className="bg-cyan-500 text-white">
               <tr>
-                <th className="py-4 px-6 text-sm sm:text-base">Obdobie</th>
-                <th className="py-4 px-6 text-sm sm:text-base">
-                  1 noc*
-                </th>
-                <th className="py-4 px-6 text-sm sm:text-base">10+ nocí</th>
-                <th className="py-4 px-6 text-sm sm:text-base">30+ nocí</th>
-                <th className="py-4 px-6 text-sm sm:text-base">
-                  Odvoz**
-                </th>
+                <th className="py-4 px-4 text-sm sm:text-base">Obdobie</th>
+                <th className="py-4 px-4 text-sm sm:text-base">1 noc*</th>
+                <th className="py-4 px-4 text-sm sm:text-base">10+ nocí</th>
+                <th className="py-4 px-4 text-sm sm:text-base">30+ nocí</th>
+                <th className="py-4 px-4 text-sm sm:text-base">Odvoz**</th>
               </tr>
             </thead>
             <tbody>
@@ -92,7 +88,6 @@ const Pricelist = () => {
           </table>
         </div>
       </motion.div>
-
       {/* Notes Below the Table */}
       <motion.div
         className="max-w-lg lg:max-w-[700px] mx-auto text-gray-600 mt-4 px-4 sm:px-6 text-sm sm:text-base"
