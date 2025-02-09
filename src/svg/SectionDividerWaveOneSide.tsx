063c351376b28b17a9d8ce9d2ae85a08727f0b74
milanuhrin <milan.uhrin19@gmail.com>
2025-02-09 16:13:44 +0100

    wave

--- src/svg/SectionDividerWaveOneSide.tsx
+++ src/svg/SectionDividerWaveOneSide.tsx
@@ -9,7 +9,7 @@ export const SectionDividerWaveOneSide = ({ fill = 'white' }: { fill?: string })
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 120"
         preserveAspectRatio="none"
-        className="w-full h-[40px] sm:h-[30px] md:h-[50px] lg:h-[70px] xl:h-[90px]"
+        className="w-full h-[40px] md:h-[50px] lg:h-[70px] xl:h-[90px]"
       >
         <path
           d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
