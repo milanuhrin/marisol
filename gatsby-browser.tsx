c54f68c1deeedb658b70d1f058bd326fd402380c
milanuhrin <milan.uhrin19@gmail.com>
2025-02-09 07:08:25 +0100

    gatsby remove fbclid

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,10 +1,25 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 interface WrapRootElementProps {
   element: ReactNode;
 }
 
-export const wrapRootElement = ({ element }: WrapRootElementProps) => (
-  <BrowserRouter>{element}</BrowserRouter>
-);
+// Function to remove fbclid from the URL
+const removeFbclid = () => {
+  if (typeof window !== "undefined") {
+    const url = new URL(window.location.href);
+    if (url.searchParams.has("fbclid")) {
+      url.searchParams.delete("fbclid"); // Remove the fbclid parameter
+      window.history.replaceState({}, document.title, url.pathname + url.search);
+    }
+  }
+};
+
+export const wrapRootElement = ({ element }: WrapRootElementProps) => {
+  useEffect(() => {
+    removeFbclid();
+  }, []);
+
+  return <BrowserRouter>{element}</BrowserRouter>;
+};
