8a0bb9c54c5c17f5805db8a9afb847f747f17a22
milanuhrin <milan.uhrin19@gmail.com>
2025-02-13 17:16:58 +0100

    delete cognito auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,6 @@ import Pricelist from "Components/Pricelist";
 import Reservation from "Components/Reservation";
 import Contact from "Components/Contact";
 import SEO from "Components/SEO";
-import { AuthProvider } from "react-oidc-context";
-
-// ✅ AWS Cognito Auth Config
-const cognitoAuthConfig = {
-  authority: "https://marisol.auth.us-east-1.amazoncognito.com",
-  // client_id: "7ha456rvc4bc7c7ve527eqrpie", // Replace with your actual Cognito App Client ID
-  client_id: "hrdsud6flksjbei479jcadat0",
-  redirect_uri: "https://main.d39j8o309sk3xb.amplifyapp.com/",
-  response_type: "code",
-  scope: "email openid profile",
-};
 
 const IndexPage = () => {
   const [isClient, setIsClient] = useState(false);
@@ -36,30 +25,28 @@ const IndexPage = () => {
   const { BrowserRouter, Routes, Route } = require("react-router-dom");
 
   return (
-    <AuthProvider {...cognitoAuthConfig}>
-      <>
-        <SEO />
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Landing />
-                  <About />
-                  <Gallery />
-                  <Pricelist />
-                  <Availability />
-                  <Reservation />
-                  <Contact />
-                  <Footer />
-                </>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </>
-    </AuthProvider>
+    <>
+      <SEO />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Landing />
+                <About />
+                <Gallery />
+                <Pricelist />
+                <Availability />
+                <Reservation />
+                <Contact />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
