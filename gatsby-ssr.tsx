148b5ecd52843e1dcdd44b113aacac47016e3389
milanuhrin <milan.uhrin19@gmail.com>
2025-02-25 08:48:16 +0100

    gatsby update

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { GatsbySSR } from "gatsby";
 
 export const wrapRootElement = ({ element }: { element: React.ReactNode }) => {
   return element; // No BrowserRouter to prevent SSR issues
+};
+
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) => {
+  setHeadComponents([
+    // ✅ Add Google Analytics Script
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-3FPGJMG229"
+      key="google-analytics"
+    />,
+    <script
+      key="google-analytics-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3FPGJMG229');
+        `,
+      }}
+    />,
+  ]);
 };
