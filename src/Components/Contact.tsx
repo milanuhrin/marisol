01c7704c407158b92d1c8c84959e7e02b750ccb6
milanuhrin <milan.uhrin19@gmail.com>
2025-03-04 23:51:28 +0100

    contact fix

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -75,16 +75,13 @@ export const Contact = () => {
           className="flex items-center space-x-2 text-green-600 text-lg font-semibold"
           whileHover={{ scale: 1.1 }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 32 32"
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+            alt="WhatsApp"
             width="24"
             height="24"
-            fill="currentColor"
-            className="text-green-600"
-          >
-            <path d="M16 2.7C8.4 2.7 2.2 8.9 2.2 16.5c0 2.7.7 5.2 2.1 7.4L2 30l6.4-2.1c2.2 1.2 4.6 1.8 7.1 1.8 7.6 0 13.8-6.2 13.8-13.8S23.6 2.7 16 2.7zm0 25.1c-2.3 0-4.5-.6-6.4-1.7l-.5-.3-3.8 1.2 1.3-3.7-.3-.6c-1.2-1.9-1.8-4.2-1.8-6.5 0-6.6 5.4-12 12-12s12 5.4 12 12-5.4 12-12 12zm5-7.8c-.3-.2-1.8-.9-2.1-1-.3-.1-.5-.1-.8.1s-.9 1.1-1.2 1.5c-.2.3-.4.3-.8.2-1.5-.5-2.8-1.6-3.7-2.9-.4-.6-.7-1.3-1-2-.2-.3-.1-.6.2-.8.3-.3.6-.8.8-1.2.2-.4.1-.7 0-1-.1-.3-.8-1.8-1.1-2.4s-.6-.5-.8-.5H8.2c-.3 0-.7.1-1 .4-.3.3-1.3 1.3-1.3 3.1s1.3 3.5 1.5 3.8c.2.3 2.7 4.1 6.5 5.8 4 .2 6.2-2 6.5-2.2s1.8-1.4 1.8-2.7c.1-1.2-.2-1.7-.3-1.9z"></path>
-          </svg>
+            className="w-6 h-6"
+          />
           <span>Napíšte nám na WhatsApp</span>
         </motion.a>
 
